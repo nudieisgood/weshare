6764a5595cde1c80e751a4a276ee844948ea91ef
nudieisgood <nudieisgoodmii@gmail.com>
2023-11-12 13:54:36 +0800

    23/11/12 fix upload issue

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -3,7 +3,7 @@ import multer from "multer";
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     // set the directory where uploaded files will be stored
-    cb(null, "public/uploads");
+    cb(null, "client/dist");
   },
   filename: (req, file, cb) => {
     const fileName = file.originalname;
