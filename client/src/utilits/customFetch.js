5c1173c797665b8afce33582620b6f343262555a
nudieisgood <nudieisgoodmii@gmail.com>
2023-11-12 12:27:42 +0800

    23/11/12 change production

--- client/src/utilits/customFetch.js
+++ client/src/utilits/customFetch.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 //development
 // export const backendBaseURL = "http://127.0.0.1:5200";
-export const backendBaseURL = "http://localhost:5200";
+// export const backendBaseURL = "http://localhost:5200";
 
 //production
-// export const backendBaseURL = "https://weshare-toghter.onrender.com";
+export const backendBaseURL = "https://weshare-toghter.onrender.com";
 
 const customFetch = axios.create({
   //development
-  baseURL: `${backendBaseURL}/api/v1`,
+  // baseURL: `${backendBaseURL}/api/v1`,
 
   //production
-  // baseURL: "/api/v1",
+  baseURL: "/api/v1",
 
   withCredentials: true,
 });
